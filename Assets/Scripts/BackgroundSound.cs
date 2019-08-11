1dffcb13dd9e37a110b1ad58c67f052ac386bc60
Joey ter Meulen <joeytermeulen@gmail.com>
2019-08-11 23:23:32 +0200

    add main menu

---
+++ Assets/Scripts/BackgroundSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSound : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        Debug.Log(GetComponent<AudioSource>().clip);
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [Header("SplashScreen details")]
+    [SerializeField] int SplashScreenLoadTime = 1;
+    [SerializeField] Slider loaderSlider;
+
+    private int currentScene;
+
+    private void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        if(currentScene == 0)
+        {
+            StartCoroutine(SplashScreenLoader());
+        }
+    }
+
+    private void Update()
+    {
+        if (currentScene == 0 && loaderSlider)
+        {
+            SplashScreenLoadBar();
+        }
+        
+    }
+
+    private void SplashScreenLoadBar()
+    {
+        loaderSlider.value += (Time.deltaTime / SplashScreenLoadTime);
+    }
+
+    IEnumerator SplashScreenLoader()
+    {
+        yield return new WaitForSeconds(SplashScreenLoadTime);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentScene + 1);
+    }
+}
