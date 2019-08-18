00134ef6a0172615dab4b2e5d929e57d4cdca400
Joey ter Meulen <joeytermeulen@gmail.com>
2019-08-18 21:58:14 +0200

    Health to attackers and vfx on hit

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,13 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
     [Range(0f,5f)] [SerializeField] float movementSpeed = 1f;
+	[SerializeField] float health = 50f;
+	[SerializeField] GameObject HitVFX;
 
     private void Update()
     {
         transform.Translate(Vector2.left * Time.deltaTime * movementSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var projectile = otherCollider.GetComponent<Projectile>();
+
+        if(projectile)
+        {
+            health -= projectile.GetDamage();
+            Destroy(otherCollider.gameObject);
+            PlayVFX();
+        }
+        
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void PlayVFX()
+    {
+        if(!HitVFX) { return; }
+
+        GameObject bloodVFX = Instantiate(HitVFX, transform.position, transform.rotation);
+        Destroy(bloodVFX, 1f);
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
+    [SerializeField] int damage = 5;
 
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    public int GetDamage()
+    {
+        return damage;
     }
 }
