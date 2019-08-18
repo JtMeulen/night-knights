d93163c1b1ec7e4d410b876315f07b99351efa78
Joey ter Meulen <joeytermeulen@gmail.com>
2019-08-18 20:23:29 +0200

    Add attackers and defenders with animations

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectile, dispenser;
+
+    public void Shoot()
+    {
+        Instantiate(projectile, dispenser.transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("Spawning Config")]
+    [SerializeField] GameObject attackerPrefab;
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+
+    private bool spawning = true;
+
+    IEnumerator Start()
+    {
+        while (spawning)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+}
