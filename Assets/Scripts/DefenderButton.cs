b542201f2201d062c40f5034adeb153c18f23734
Joey ter Meulen <joeytermeulen@gmail.com>
2019-08-19 22:04:55 +0200

    Add spawning of defenders in tile, UI for defender buttons

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    Color32 darkColor = new Color32(50, 50, 50, 255);
+    Color32 whiteColor = new Color32(255, 255, 255, 255);
+
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<DefenderButton>();
+
+        foreach (DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = darkColor;
+        }
+
+        GetComponent<SpriteRenderer>().color = whiteColor;
+    }
+}
---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defender;
+
+    private void OnMouseDown()
+    {
+       Instantiate(defender, GetSquareClicked(), Quaternion.identity);
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        worldPos.x = Mathf.Round(worldPos.x);
+        worldPos.y = Mathf.Round(worldPos.y);
+        return worldPos;
+    }
+}
